90f865a712e7360ffc8a63aaedd3f7a24fdb46cf
unknown <dorultanianosgyorgy@gmail.com>
2020-01-02 20:11:27 +0000

    s

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -5,6 +5,7 @@ const StartServerPlugin = require('start-server-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const DotEnvPlugin = require('dotenv-webpack');
 
 const serverConfig = {
 	name: 'server',
@@ -22,6 +23,7 @@ const serverConfig = {
 	    aggregateTimeout: 300,
 	    poll: 1000
 	},
+
 	module: {
 		rules: sharedConfig.module.rules
 	},
@@ -31,16 +33,18 @@ const serverConfig = {
 		new CleanWebpackPlugin('./dist'),
 		new StartServerPlugin('server.js'),
     new webpack.optimize.OccurrenceOrderPlugin(),
+		new DotEnvPlugin(),
     new webpack.DefinePlugin({
         "process.env": {
             "BUILD_TARGET": JSON.stringify('server'),
-						"PORT": JSON.stringify(process.env.PORT),
+						"PORT": JSON.stringify(process.env.PORT) || 3000,
 						"DB_USERNAME":  JSON.stringify(process.env.DB_USERNAME),
 						"DB_PASSWORD":  JSON.stringify(process.env.DB_PASSWORD),
 						"DB_HOST":  JSON.stringify(process.env.DB_HOST),
 						"DB_NAME":  JSON.stringify(process.env.DB_NAME)
         }
     }),
+
     new webpack.NoEmitOnErrorsPlugin(),
 		new ProgressBarPlugin(),
 		...sharedConfig.plugins
