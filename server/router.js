61ceefe701191f83a1589c31f93d609617c2d477
unknown <dorultanianosgyorgy@gmail.com>
2020-01-13 10:57:54 +0000

    google analytics

--- server/router.js
+++ server/router.js
@@ -41,6 +41,16 @@ const renderHtml = (html, content, INIT_DATA, extractor) => {
 
 			${extractor.getScriptTags()}
 		<script>const INIT_DATA = ${JSON.stringify(INIT_DATA)};</script>
+			<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-156100886-1"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'UA-156100886-1');
+		</script>
+
 		</body>
 	 </html>
 	`
